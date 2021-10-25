5f84cde597375b9f2ce4a3b47d6f6265498a0703
harits <86910732+whoarewe22@users.noreply.github.com>
2021-10-25 10:42:33 +0700

    Add files via upload

--- Latihan1.php
+++ Latihan1.php
@@ -1,25 +1,17 @@
-<?php
+<html>
 
-namespace App\Controllers;
+<head>
+    <title>Latihan1</title>
+</head>
 
-class Latihan1 extends BaseController
-{
-    public function index()
-    {
-        echo "Selamat Datang.. selamat belajar Web Programming";
-    }
-    //$this->load->view('view-latihan1'); 
-    public function penjumlahan($n1, $n2)
-    {
-        $Latihan_model1 = new \App\Models\Latihan_model1();
-        $data['nilai1'] = $n1;
-        $data['nilai2'] = $n2;
-        $data['hasil'] = $Latihan_model1->jumlah($n1, $n2);
-        echo view('Latihan1', $data);
-    }
-    public function nama()
-    {
-        $this->nama = "harits";
-        echo "nama saya $this->nama.";
-    }
-}
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu
+    <?= $nilai1 .
+        " + " . $nilai2 .
+        " = " . $hasil; ?>
+</body>
+
+</html>
